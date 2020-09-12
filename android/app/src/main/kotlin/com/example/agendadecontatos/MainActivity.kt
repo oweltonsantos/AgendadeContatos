c930f62cf3d8d6f3be163106e45da5ad01a15dac
oweltonsantos <oweltonsantos@gmail.com>
2020-09-11 21:06:36 -0300

    CF - Agenda_de_Contatos

---
+++ android/app/src/main/kotlin/com/example/agendadecontatos/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.agendadecontatos
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:agendadecontatos/helpers/contact_helper.dart';
+import 'package:agendadecontatos/ui/contact_page.dart';
+import 'package:agendadecontatos/ui/home_page.dart';
+import 'package:flutter/material.dart';
+
+
+
+void main () {
+runApp(MaterialApp(
+  home: HomePage(),
+  debugShowCheckedModeBanner: false,
+
+));
+}
+
+
